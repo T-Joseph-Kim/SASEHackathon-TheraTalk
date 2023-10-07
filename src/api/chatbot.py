cc7fec5bb67c88998da42052a32e80320f462966
manasadepu <112828012+manasadepu@users.noreply.github.com>
2023-10-07 15:59:28 -0400

    added chatbot without app wrapper

--- src/api/chatbot.py
+++ src/api/chatbot.py
@@ -1,8 +1,63 @@
+from dotenv import load_dotenv
+load_dotenv()
 import os
 
-os.environ["OPEN_API_KEY"] = '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'
-
+print(os.environ.get("OPENAI_API_KEY"))
 from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
-documents =SimpleDirectoryReader('/Users/manasadepu/Documents/GitHub/SASEHACK-Mental-Health-/datasets/Mental_Health_FAQ.csv').load_data()
-print(documents)
-print('hello')
+documents =SimpleDirectoryReader('/Users/manasadepu/Documents/GitHub/SASEHACK-Mental-Health-/datasets').load_data()
+
+index = GPTVectorStoreIndex.from_documents(documents)
+
+
+# Save the index
+index.storage_context.persist('/Users/manasadepu/Documents/GitHub/SASEHACK-Mental-Health-/src/api')
+
+# Chat Bot
+
+import openai
+import json
+
+# Inspired by Vidhya Analytics
+
+class Chatbot:
+    def __init__(self, api_key, index):
+        self.index = index
+        openai.api_key = api_key
+        self.chat_history = []
+
+    def generate_response(self, user_input):
+        prompt = "\n".join([f"{message['role']}: {message['content']}" 
+                           for message in self.chat_history[-5:]])
+        prompt += f"\nUser: {user_input}"
+        query_engine = index.as_query_engine()
+        response = query_engine.query(user_input)
+
+        message = {"role": "assistant", "content": response.response}
+        self.chat_history.append({"role": "user", "content": user_input})
+        self.chat_history.append(message)
+        return message
+
+    def load_chat_history(self, filename):
+        try:
+            with open(filename, 'r') as f:
+                self.chat_history = json.load(f)
+        except FileNotFoundError:
+            pass
+
+    def save_chat_history(self, filename):
+        with open(filename, 'w') as f:
+            json.dump(self.chat_history, f)
+
+
+bot = Chatbot(os.environ.get("OPENAI_API_KEY"), index=index)
+bot.load_chat_history("chat_history.json")
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["bye", "goodbye"]:
+        print("Bot: Goodbye!")
+        bot.save_chat_history("chat_history.json")
+        break
+    response = bot.generate_response(user_input)
+    print(f"Bot: {response['content']}")
+
