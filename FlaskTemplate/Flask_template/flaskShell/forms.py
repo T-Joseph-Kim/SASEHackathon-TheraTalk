02fb7466472d5ff41bda027c213a4ed290218807
RunningUpTheHill <tianzhong.j@ufl.edu>
2023-10-08 23:26:13 -0400

    Fully Updated with UX and journal posts

--- FlaskTemplate/Flask_template/flaskShell/forms.py
+++ FlaskTemplate/Flask_template/flaskShell/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskShell.models import User
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -53,3 +54,9 @@ class UpdateForm(FlaskForm):
             email = User.query.filter_by(email=email.data).first()
             if email: 
                 raise ValidationError("Email has been used. Please choose a different one.")
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
--- FlaskTemplate/Flask_template/flaskShell/routes.py
+++ FlaskTemplate/Flask_template/flaskShell/routes.py
@@ -1,27 +1,26 @@
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 import secrets
 import os
 from PIL import Image
 from flaskShell import app, db, bcrypt
-from flaskShell.forms import RegistrationForm, LoginForm, UpdateForm
-from flaskShell.models import User, Conversation
+from flaskShell.forms import RegistrationForm, LoginForm, UpdateForm, PostForm
+from flaskShell.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
 
 creators = [
     {
         "Name" : "Jun Tianzhong",
-        "Title" : "Front-End"
+        "Title" : "Frontend / Backend"
     },
 
     {
         "Name" : "Joseph Kim",
-        "Title" : "Login Feature"
+        "Title" : "User Interface"
     },
 
     {
         "Name" : "Manas Adepu", 
-        "Title" : "ChatBot"
+        "Title" : "ChatBot / Journal"
     }
 ]
 
@@ -29,7 +28,9 @@ creators = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    posts = Post.query.all()
+    return render_template('home.html', posts=posts)
+
 
 
 @app.route("/about")
@@ -107,3 +108,53 @@ def account():
     return render_template('account.html', title='Account', image_file=image_file, form=form)
 
 
+@app.route("/post_new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, Name=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Journal',
+                           form=form, legend='New Journal')
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.Name != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.Name != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
