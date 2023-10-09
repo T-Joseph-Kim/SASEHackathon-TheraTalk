d49ae3d9819744553b9c6aa7249f115c7211393c
Manas A <112828012+manasadepu@users.noreply.github.com>
2023-10-08 22:41:47 -0400

    Updated HTML for UI features (#2)
    Co-authored-by: RunningUpTheHill <tianzhong.j@ufl.edu>

--- FlaskTemplate/Flask_template/flaskShell/routes.py
+++ FlaskTemplate/Flask_template/flaskShell/routes.py
@@ -34,12 +34,12 @@ creators = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", creators=creators)
+    return render_template("home.html")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title="about")
+    return render_template("about.html", creators=creators)
 
 
 @app.route("/register", methods=['GET', 'POST'])
